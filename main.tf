30437c4e4cad20986e269c9b67140a67ff0e5a13
R-tooR <arturbugaj37@gmail.com>
2022-05-13 09:04:53 +0200

    cluster ca certificate

--- main.tf
+++ main.tf
@@ -25,24 +25,24 @@ resource "azurerm_kubernetes_cluster" "ms-up-running" {
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dns_prefix
 
-#  service_principal {
-#    client_id     = var.service_principal_id
-#    client_secret = var.service_principal_secret
-#  }
+  #  service_principal {
+  #    client_id     = var.service_principal_id
+  #    client_secret = var.service_principal_secret
+  #  }
   identity {
     type = "SystemAssigned"
   }
 
   default_node_pool {
-    name                  = local.pool_name
-    vm_size               = var.nodegroup_size # Standard_B2s
-    max_pods              = var.nodegroup_max_pod_size
-    max_count             = var.nodegroup_max_size
-    min_count             = var.nodegroup_min_size
-    type                  = "VirtualMachineScaleSets"
-    os_disk_size_gb       = var.nodegroup_disk_size # osDiskSizeGB
-#    enable_node_public_ip = true
-    enable_auto_scaling   = true
+    name            = local.pool_name
+    vm_size         = var.nodegroup_size # Standard_B2s
+    max_pods        = var.nodegroup_max_pod_size
+    max_count       = var.nodegroup_max_size
+    min_count       = var.nodegroup_min_size
+    type            = "VirtualMachineScaleSets"
+    os_disk_size_gb = var.nodegroup_disk_size # osDiskSizeGB
+    #    enable_node_public_ip = true
+    enable_auto_scaling = true
   }
 
   network_profile {
@@ -54,22 +54,22 @@ resource "azurerm_kubernetes_cluster" "ms-up-running" {
 
 resource "local_file" "kubeconfig" {
   filename = "kubeconfig"
-  content = azurerm_kubernetes_cluster.ms-up-running.kube_config_raw
-#  content  = <<KUBECONFIG_END
-#apiVersion: v1
-#clusters:
-#- cluster:
-#    "certificate-authority-data: >
-#    ${azurerm_kubernetes_cluster.ms-up-running.kube_config.0.client_certificate}"
-#    server: ${azurerm_kubernetes_cluster.ms-up-running.dns_prefix}
-#  name: ${azurerm_kubernetes_cluster.ms-up-running.name}
-#contexts:
-#- context:
-#    cluster: ${azurerm_kubernetes_cluster.ms-up-running.name}
-#    user: ${azurerm_kubernetes_cluster.ms-up-running.name}
-#  name: ${azurerm_kubernetes_cluster.ms-up-running.name}
-#current-context: ${azurerm_kubernetes_cluster.ms-up-running.name}
-#kind: Config
-#preferences: {}
-#  KUBECONFIG_END
+  content  = azurerm_kubernetes_cluster.ms-up-running.kube_config_raw
+  #  content  = <<KUBECONFIG_END
+  #apiVersion: v1
+  #clusters:
+  #- cluster:
+  #    "certificate-authority-data: >
+  #    ${azurerm_kubernetes_cluster.ms-up-running.kube_config.0.client_certificate}"
+  #    server: ${azurerm_kubernetes_cluster.ms-up-running.dns_prefix}
+  #  name: ${azurerm_kubernetes_cluster.ms-up-running.name}
+  #contexts:
+  #- context:
+  #    cluster: ${azurerm_kubernetes_cluster.ms-up-running.name}
+  #    user: ${azurerm_kubernetes_cluster.ms-up-running.name}
+  #  name: ${azurerm_kubernetes_cluster.ms-up-running.name}
+  #current-context: ${azurerm_kubernetes_cluster.ms-up-running.name}
+  #kind: Config
+  #preferences: {}
+  #  KUBECONFIG_END
 }
--- outputs.tf
+++ outputs.tf
@@ -6,8 +6,8 @@ output "kubernetes_cluster_id" {
   value = azurerm_kubernetes_cluster.ms-up-running.id
 }
 
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.ms-up-running.kube_config.0.client_certificate
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.ms-up-running.kube_config.0.cluster_ca_certificate
 }
 
 output "aks_cluster_endpoint" {
